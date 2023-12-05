1836f78e9a2f0e4e5fac17676c9b0e5a1d754553
Tim Jones <timniverse@gmail.com>
2023-12-05 23:39:31 +0100

    Add a custom ISO installer from nix-generators.

--- Makefile
+++ Makefile
@@ -2,11 +2,6 @@
 help: ## Prints help for targets with comments
 	@cat $(MAKEFILE_LIST) | grep -E '^[a-zA-Z_-]+:.*?## .*$$' | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
 
-.PHONY: install
-install-%: ## Installs NixOS for a given box
-	sudo -E nix run github:nix-community/disko -- --mode disko boxen/$@/disko.nix
-	sudo -E nixos-install --no-root-passwd --flake .#$*
-
 .PHONY: build
 build-%: ## Builds the NixOS system for a given box
 	sudo -E nixos-rebuild switch --flake .#$*
@@ -14,3 +9,7 @@ build-%: ## Builds the NixOS system for a given box
 .PHONY: rehome
 rehome-%: ## Uses home-manager to reconfigure the home env
 	home-manager switch --flake .#$*
+
+.PHONY: iso
+iso-%:  ## Builds a minimal installer ISO for a given box
+	nix build .#iso-$*
--- flake.nix
+++ flake.nix
@@ -16,6 +16,10 @@
       url = "github:nix-community/nixvim/nixos-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nixos-generators = {
+      url = "github:nix-community/nixos-generators";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
@@ -25,6 +29,7 @@
     disko,
     home-manager,
     nixvim,
+    nixos-generators,
     ...
   } @ inputs: let
     inherit (self) outputs;
@@ -50,6 +55,40 @@
 	   nixvim.homeManagerModules.nixvim
 	  ./home/tim
 	];
+      };
+    };
+
+    packages.x86_64-linux = {
+      iso-laptop-02 = nixos-generators.nixosGenerate {
+        format = "install-iso";
+	system = "x86_64-linux";
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+	modules = [
+	  disko.nixosModules.disko
+	  ./boxen/laptop-02/disko.nix
+	  ({ config, pkgs, ...}: let
+	    systemDev = self.nixosConfigurations."laptop-02".config.system.build.toplevel;
+	    diskoScript = pkgs.writeShellScriptBin "disko" "${config.system.build.diskoScript}";
+	    installScript = pkgs.writeShellScriptBin "install-system" ''
+	      set -euo pipefail
+
+	      echo "Setting up disks..."
+	      . ${diskoScript}/bin/disko
+
+	      echo "Installing system..."
+	      nixos-install --no-root-passwd --system ${systemDev}
+
+	      echo "Done!"
+	    '';
+	  in {
+	    # Don't generate the filesystem config since it's for the target rather than the installer ISO
+            disko.enableConfig = false;
+	    environment.systemPackages = [
+	      diskoScript
+	      installScript
+            ];
+	  })
+        ];
       };
     };
   };
