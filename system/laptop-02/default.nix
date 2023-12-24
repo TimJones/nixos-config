68e5c209388a63df14746c0ab58bdba42beb8fae
Tim Jones <timniverse@gmail.com>
2023-12-24 16:07:21 +0100

    Add initrd commands to restore clean snapshots.

--- system/laptop-02/default.nix
+++ system/laptop-02/default.nix
@@ -1,4 +1,7 @@
 {
+  lib,
+  ...
+}: {
   imports = [
     ./disk.nix
   ];
@@ -24,6 +27,15 @@
     initrd = {
       availableKernelModules = [ "nvme" "xhci_pci" "thunderbolt" "usb_storage" "sd_mod" ];
       kernelModules = [ ];
+      postDeviceCommands = lib.mkAfter ''
+        (
+          MNTPOINT=$(mktemp -d)
+          mount /dev/disk/by-partlabel/disk-main-OS "$MNTPOINT" -o subvol=/
+          trap 'umount $MNTPOINT; rm -rf $MNTPOINT' EXIT
+          btrfs subvolume delete "$MNTPOINT"/rootfs*
+          btrfs subvolume snapshot "$MNTPOINT"/rootfs-clean "$MNTPOINT"/rootfs
+	)
+      '';
     };
   };
 
