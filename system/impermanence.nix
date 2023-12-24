efc0f24da1d181d8d8b33b1e251e031bfed96878
Tim Jones <timniverse@gmail.com>
2023-12-24 17:43:23 +0100

    Add permanence for SSH host identity.

--- system/impermanence.nix
+++ system/impermanence.nix
@@ -2,6 +2,8 @@
   environment.persistence."/persist" = {
     files = [
       "/etc/machine-id"
+      "/etc/ssh/ssh_host_rsa_key"
+      "/etc/ssh/ssh_host_ed25519_key"
     ];
     directories = [
       "/var/log"
