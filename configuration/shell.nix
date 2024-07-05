3d07fbe7bc393d0f7dce9f0641720c36ce78c2fa
Tim Jones <timniverse@gmail.com>
2024-07-05 11:54:15 +0200

    Use direnv

--- configuration/shell.nix
+++ configuration/shell.nix
@@ -1,22 +1,35 @@
 {
   programs.zsh.enable = true;
 
-  environment.persistence."/persist".users.tim.files = [
-    ".zsh_history"
-  ];
+  home-manager.users.tim.programs = {
+    direnv = {
+      enable = true;
+      enableZshIntegration = true;
+      nix-direnv.enable = true;
+    };
 
-  home-manager.users.tim.programs.zsh = {
-    enable = true;
-    autocd = true;
-    enableCompletion = true;
-    enableVteIntegration = true;
-    autosuggestion.enable = true;
+    zsh = {
+      enable = true;
+      autocd = true;
+      enableCompletion = true;
+      enableVteIntegration = true;
+      autosuggestion.enable = true;
 
-    history.ignoreAllDups = true;
+      history.ignoreAllDups = true;
 
-    dirHashes = {
-      personal = "~/projects/personal";
-      work = "~/projects/siderolabs";
+      dirHashes = {
+        personal = "~/projects/personal";
+        work = "~/projects/siderolabs";
+      };
     };
   };
-}  
+
+  environment.persistence."/persist".users.tim = {
+    files = [
+      ".zsh_history"
+    ];
+    directories = [
+      ".local/share/direnv"
+    ];
+  };
+}
