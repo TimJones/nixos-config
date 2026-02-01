782d625d0eff9408a567223eb0074c3fff79d59f
Tim Jones <timniverse@gmail.com>
2026-02-01 10:05:53 +0100

    Start from scratch with dendriditc flake-file template

--- flake.nix
+++ flake.nix
@@ -1,77 +1,19 @@
+# DO-NOT-EDIT. This file was auto-generated using github:vic/flake-file.
+# Use `nix run .#write-flake` to regenerate it.
 {
-  description = "NixOS config for Tim";
+
+  outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./modules);
 
   inputs = {
+    flake-file.url = "github:vic/flake-file";
+    flake-parts = {
+      inputs.nixpkgs-lib.follows = "nixpkgs-lib";
+      url = "github:hercules-ci/flake-parts";
+    };
+    import-tree.url = "github:vic/import-tree";
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    nixos-hardware.url = "github:nixos/nixos-hardware";
-    impermanence.url = "github:nix-community/impermanence";
-    sops-nix.url = "github:Mic92/sops-nix";
-    disko = {
-      url = "github:nix-community/disko";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    nixvim = {
-      url = "github:nix-community/nixvim";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nixpkgs-lib.follows = "nixpkgs";
+    systems.url = "github:nix-systems/default";
   };
 
-  outputs =
-    { self
-    , nixpkgs
-    , nixos-hardware
-    , disko
-    , home-manager
-    , nixvim
-    , impermanence
-    , sops-nix
-    , ...
-    } @ inputs:
-    let
-      inherit (self) outputs;
-    in
-    {
-      formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt;
-
-      nixosConfigurations = {
-        laptop-02 = nixpkgs.lib.nixosSystem {
-          specialArgs = {
-            inherit inputs outputs;
-            mainDisk = "/dev/disk/by-id/nvme-WD_BLACK_SN850X_4000GB_23270P802370";
-            hostName = "laptop-02";
-          };
-          modules = [
-            nixos-hardware.nixosModules.framework-13-7040-amd
-            disko.nixosModules.disko
-            impermanence.nixosModules.impermanence
-            sops-nix.nixosModules.sops
-            nixvim.nixosModules.nixvim
-            ./hardware
-            ./configuration
-          ];
-        };
-
-        desktop-02 = nixpkgs.lib.nixosSystem {
-          specialArgs = {
-            inherit inputs outputs;
-            mainDisk = "/dev/disk/by-id/nvme-WD_BLACK_SN850X_4000GB_23513M801557";
-            hostName = "desktop-02";
-          };
-          modules = [
-            nixos-hardware.nixosModules.common-cpu-amd
-            nixos-hardware.nixosModules.common-gpu-amd
-            disko.nixosModules.disko
-            impermanence.nixosModules.impermanence
-            sops-nix.nixosModules.sops
-            nixvim.nixosModules.nixvim
-            ./hardware
-            ./configuration
-          ];
-        };
-      };
-    };
 }
