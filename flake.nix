6ac02a37d518e93ab2cf3db616490cba270e69fa
Tim Jones <timniverse@gmail.com>
2026-03-01 12:11:50 +0100

    Add disko for filesystem management

--- flake.nix
+++ flake.nix
@@ -5,13 +5,17 @@
   outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./modules);
 
   inputs = {
+    disko = {
+      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:nix-community/disko/latest";
+    };
     flake-file.url = "github:vic/flake-file";
     flake-parts = {
       inputs.nixpkgs-lib.follows = "nixpkgs-lib";
       url = "github:hercules-ci/flake-parts";
     };
     import-tree.url = "github:vic/import-tree";
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-lib.follows = "nixpkgs";
     sops-nix = {
       inputs.nixpkgs.follows = "nixpkgs";
---
+++ modules/disko.nix
@@ -0,0 +1,90 @@
+{
+  inputs,
+  ...
+}:
+{
+  flake-file.inputs.disko = {
+    url = "github:nix-community/disko/latest";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  flake.modules.nixos.disko = {
+    imports = [
+      inputs.disko.nixosModules.disko
+    ];
+  };
+
+  flake.factory.diskoMainDevice =
+    {
+      mainDevice,
+      swapSize ? "2G",
+      encrypt ? false,
+    }:
+    let
+      btrfsContent = {
+        type = "btrfs";
+        subvolumes = {
+          root = {
+            mountpoint = "/";
+            mountOptions = [
+              "compress=zstd"
+              "noatime"
+              "nodiratime"
+            ];
+          };
+          nix = {
+            mountpoint = "/nix";
+            mountOptions = [
+              "compress=zstd"
+              "noatime"
+              "nodiratime"
+            ];
+          };
+          home = {
+            mountpoint = "/home";
+            mountOptions = [
+              "compress=zstd"
+              "noatime"
+              "nodiratime"
+            ];
+          };
+          swap = {
+            mountpoint = "/swap";
+            swap.swapfile.size = swapSize;
+          };
+        };
+      };
+      luksContent = {
+        type = "luks";
+        name = "crypted";
+        settings.allowDiscards = true;
+        content = btrfsContent;
+      };
+    in
+    {
+      disko.devices.disk.main = {
+        type = "disk";
+        device = mainDevice;
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              type = "EF00";
+              size = "500M";
+              priority = 1;
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
+              };
+            };
+            OS = {
+              size = "100%";
+              content = if encrypt then luksContent else btrfsContent;
+            };
+          };
+        };
+      };
+    };
+}
