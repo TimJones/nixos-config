c5aa5bb4bd320629f021489fdec9c08b6e5c39e3
Tim Jones <timniverse@gmail.com>
2026-05-17 15:28:12 +0200

    Add tim user

---
+++ modules/users/tim/user.nix
@@ -0,0 +1,31 @@
+{
+  inputs,
+  self,
+  lib,
+  ...
+
+}:
+let
+  user = "tim";
+in
+{
+  flake.modules = lib.mkMerge [
+    (self.factory.user {
+      username = "${user}";
+      isAdmin = true;
+      hasSSH = true;
+      hasPwdHash = true;
+    })
+    {
+      homeManager."${user}" = {
+        imports = with inputs.self.modules.homeManager; [
+          system-minimal
+        ];
+        home.username = "${user}";
+
+      };
+    }
+  ];
+
+  flake.homeConfigurations = self.lib.mkHomeManager "x86_64-linux" "${user}";
+}
