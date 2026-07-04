6175eaa3d7df8a3bdcfc80ca92cbfae82271fc62
Tim Jones <timniverse@gmail.com>
2026-07-04 22:17:42 +0200

    Add zsh config for user tim

---
+++ modules/users/tim/zsh.nix
@@ -0,0 +1,36 @@
+{ den, ... }:
+{
+  den.aspects.tim.provides.zsh = {
+    includes = [
+      (den.batteries.user-shell "zsh")
+    ];
+
+    homeManager = { pkgs, ... }: {
+      programs.zsh = {
+        autocd = true;
+        enableCompletion = true;
+        enableVteIntegration = true;
+
+        history = {
+          append = true;
+          saveNoDups = true;
+          ignoreAllDups = true;
+          expireDuplicatesFirst = true;
+        };
+
+        historySubstringSearch.enable = true;
+
+        initContent = ''
+          source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
+          source ${./p10k.zsh}
+        '';
+      };
+    };
+
+    permHome = {
+      files = [
+        ".zsh_history"
+      ];
+    };
+  };
+}
