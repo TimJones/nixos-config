709a59d0d5f337e267bd26360a7e442b31b01d1b
Tim Jones <timniverse@gmail.com>
2026-07-04 16:15:57 +0200

    Add secrets for tim

--- modules/users/tim/default.nix
+++ modules/users/tim/default.nix
@@ -6,6 +6,7 @@
       den.batteries.tpm-access
       (den.batteries.user-shell "zsh")
       den.aspects.tim.ssh
+      den.aspects.host-secrets
     ];
 
     permHome = {
@@ -13,5 +14,23 @@
         "projects"
       ];
     };
+
+    nixos = {
+      users.mutableUsers = false;
+
+      sops.secrets = {
+        "tim/pwd_hash" = {
+          sopsFile = ./secrets.yaml;
+          neededForUsers = true;
+          key = "pwd_hash";
+        };
+      };
+    };
+
+    user =
+      { osConfig, ... }:
+      {
+        hashedPasswordFile = osConfig.sops.secrets."tim/pwd_hash".path;
+      };
   };
 }
--- modules/users/tim/ssh.nix
+++ modules/users/tim/ssh.nix
@@ -1,5 +1,24 @@
+{ den, ... }:
 {
   den.aspects.tim.provides.ssh = {
+    includes = [ den.aspects.host-secrets ];
+
+    # As this key is esentially the users' SOPS key, it needs to be unlocked at the host level
+    nixos =
+      { config, ... }:
+      let
+        userCfg = config.users.users.tim;
+      in
+      {
+        sops.secrets."${userCfg.name}/ssh/id_ed25519" = {
+          sopsFile = ./secrets.yaml;
+          key = "ssh/ed25519";
+          path = "${userCfg.home}/.ssh/id_ed25519";
+          owner = userCfg.name;
+          mode = "0400";
+        };
+      };
+
     user = {
       openssh.authorizedKeys.keyFiles = [ ./id_ed25519.pub ];
     };
