606ea007fe0bf8b1f19a4c0a99621ae298ea27e5
Tim Jones <timniverse@gmail.com>
2026-07-20 21:08:36 +0100

    fixup git config

--- modules/users/tim/git.nix
+++ modules/users/tim/git.nix
@@ -9,6 +9,31 @@
           name = "Tim Jones";
           email = "timniverse@gmail.com";
         };
+
+        extraConfig = {
+          init.defaultBranch = "main";
+          pull.ff = "only";
+          push = {
+            default = "current";
+            autoSetupRemote = true;
+          };
+          url."ssh://git@github.com".insteadOf = "https://github.com";
+        };
+
+        includes = [
+          {
+            condition = "gitdir:~/projects/work/";
+            contents = {
+              tag.gpgSign = true;
+              commit.gpgSign = true;
+              format.signOff = true;
+              user = {
+                email = "tim.jones@siderolabs.com";
+                signingKey = "5D7964FF2DB426ACB3C3505AA2A702DD5B689F45";
+              };
+            };
+          }
+        ];
       };
     };
   };
